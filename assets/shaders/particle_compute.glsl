491f3a74743f7b82db7ee95911ba6e9033200b9d
Amy Opas <amy.opas@proton.me>
2026-04-16 01:37:32 +0200

    tidy up

--- assets/shaders/particle_compute.glsl
+++ assets/shaders/particle_compute.glsl
@@ -1,34 +1,29 @@
-// We require version 430 since it supports compute shaders.
 #version 430
 
-// This is the workgroup size. The largest size that is guaranteed by OpenGL
-// to available is 1024, beyond this is uncertain.
-// Might influence performance but only in advanced cases.
 layout(local_size_x = 1024, local_size_y = 1, local_size_z = 1) in;
 
-layout(std430, binding = 0) buffer ssboPositions {
-    vec4 positions[];
+layout(std430, binding = 0) buffer SSBO_0 {
+    vec4 data_0[];
 };
-layout(std430, binding = 1) buffer ssboVelocities {
-    vec4 velocities[];
+layout(std430, binding = 1) buffer SSBO_1 {
+    vec4 data_1[];
 };
 
-// Uniform values are the way in which we can modify the shader efficiently.
-// These can be updated every frame efficiently.
-// We use layout(location=...) but you can also leave it and query the location in Raylib.
 layout(location = 0) uniform float deltaTime;
 layout(location = 1) uniform float speed;
 
-void main()
-{
-    uint index = gl_GlobalInvocationID.x;
+#define pos(idx)          (data_0[(idx)].xy)
+#define lifetime_sec(idx) (data_0[(idx)].x)
+
+void main() {
+    uint idx = gl_GlobalInvocationID.x;
 
     // Test upward movement
-    positions[index].x += speed * deltaTime;
+    pos(idx).x += speed * deltaTime;
 
     // Test clamp
-    if (positions[index].x > 1.0) positions[index].x = -1.0;
-    if (positions[index].x < -1.0) positions[index].x = 1.0;
+    if (pos(idx).x > 1.0) pos(idx).x = -1.0;
+    if (pos(idx).x < -1.0) pos(idx).x = 1.0;
 
     // velocities[index].xy = vec2(0.0, 0.0);
 }
--- src/particle.zig
+++ src/particle.zig
@@ -9,8 +9,8 @@ pub const ParticleData = struct {
     vao: u32,
     num_particles: u32,
     compute_shader: u32,
-    ssbo_positions: u32,
-    ssbo_velocities: u32,
+    ssbo_0: u32,
+    ssbo_1: u32,
 };
 
 pub fn init(allocator: std.mem.Allocator, rng: std.Random) ParticleData {
@@ -21,34 +21,39 @@ pub fn init(allocator: std.mem.Allocator, rng: std.Random) ParticleData {
 
     const num_particles: u32 = 1024;
 
-    const positions: []rl.Vector4 = allocator.alloc(rl.Vector4, num_particles) catch {
-        @panic("OOM");
-    };
-    defer allocator.free(positions);
+    const ssbo_0_data: []rl.Vector4 = allocator.alloc(rl.Vector4, num_particles) catch @panic("OOM");
+    defer allocator.free(ssbo_0_data);
 
-    const velocities: []rl.Vector4 = allocator.alloc(rl.Vector4, num_particles) catch {
-        @panic("OOM");
-    };
-    defer allocator.free(velocities);
+    const ssbo_1_data: []rl.Vector4 = allocator.alloc(rl.Vector4, num_particles) catch @panic("OOM");
+    defer allocator.free(ssbo_1_data);
 
     for (0..num_particles - 1) |i| {
-        positions[i] = rl.Vector4{
-            .x = helpers.randomFloatRange(rng, -1.0, 1.0),
-            .y = helpers.randomFloatRange(rng, -1.0, 1.0),
-            .z = 0.0,
-            .w = 0.0,
-        };
-        velocities[i] = rl.Vector4.init(0.0, 0.0, 0.0, 0.0);
+        const x_pos: f32 = helpers.randomFloatRange(rng, -1.0, 1.0);
+        const y_pos: f32 = helpers.randomFloatRange(rng, -1.0, 1.0);
+        const lifetime_sec: f32 = helpers.randomFloatRange(rng, 2.0, 15.0);
+
+        ssbo_0_data[i] = rl.Vector4.init(
+            x_pos,
+            y_pos,
+            lifetime_sec,
+            0.0,
+        );
+        ssbo_1_data[i] = rl.Vector4.init(
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+        );
     }
 
-    const ssbo_positions = rl.gl.rlLoadShaderBuffer(
+    const ssbo_0 = rl.gl.rlLoadShaderBuffer(
         num_particles * @sizeOf(rl.Vector4),
-        positions.ptr,
+        ssbo_0_data.ptr,
         rl.gl.rl_dynamic_copy,
     );
-    const ssbo_velocities = rl.gl.rlLoadShaderBuffer(
+    const ssbo_1 = rl.gl.rlLoadShaderBuffer(
         num_particles * @sizeOf(rl.Vector4),
-        velocities.ptr,
+        ssbo_1_data.ptr,
         rl.gl.rl_dynamic_copy,
     );
 
@@ -75,14 +80,14 @@ pub fn init(allocator: std.mem.Allocator, rng: std.Random) ParticleData {
         .vao = vao,
         .num_particles = num_particles,
         .compute_shader = compute_shader,
-        .ssbo_positions = ssbo_positions,
-        .ssbo_velocities = ssbo_velocities,
+        .ssbo_0 = ssbo_0,
+        .ssbo_1 = ssbo_1,
     };
 }
 
 pub fn deinit(data: ParticleData) void {
-    rl.gl.rlUnloadShaderBuffer(data.ssbo_positions);
-    rl.gl.rlUnloadShaderBuffer(data.ssbo_velocities);
+    rl.gl.rlUnloadShaderBuffer(data.ssbo_0);
+    rl.gl.rlUnloadShaderBuffer(data.ssbo_1);
     rl.gl.rlUnloadVertexArray(data.vao);
     rl.gl.rlUnloadShaderProgram(data.compute_shader);
 }
@@ -97,8 +102,8 @@ pub fn compute(data: *ParticleData) void {
     rl.gl.rlEnableShader(data.compute_shader);
     rl.gl.rlSetUniform(0, &dt, @intFromEnum(rl.ShaderUniformDataType.float), 1);
     rl.gl.rlSetUniform(1, &speed, @intFromEnum(rl.ShaderUniformDataType.float), 1);
-    rl.gl.rlBindShaderBuffer(data.ssbo_positions, 0);
-    rl.gl.rlBindShaderBuffer(data.ssbo_velocities, 1);
+    rl.gl.rlBindShaderBuffer(data.ssbo_0, 0);
+    rl.gl.rlBindShaderBuffer(data.ssbo_1, 1);
     rl.gl.rlComputeShaderDispatch(data.num_particles / 1024, 1, 1);
     rl.gl.rlDisableShader();
 }
@@ -112,8 +117,8 @@ pub fn draw(ctx: *Context) void {
     rl.beginShaderMode(shader);
     rl.setShaderValue(shader, 0, &particle_scale, rl.ShaderUniformDataType.float);
 
-    rl.gl.rlBindShaderBuffer(ctx.particle_data.ssbo_positions, 0);
-    rl.gl.rlBindShaderBuffer(ctx.particle_data.ssbo_velocities, 1);
+    rl.gl.rlBindShaderBuffer(ctx.particle_data.ssbo_0, 0);
+    rl.gl.rlBindShaderBuffer(ctx.particle_data.ssbo_1, 1);
 
     _ = rl.gl.rlEnableVertexArray(ctx.particle_data.vao);
     rl.gl.rlDrawVertexArrayInstanced(0, 6, @intCast(ctx.particle_data.num_particles));
